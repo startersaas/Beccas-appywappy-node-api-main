81faf02b8e83aa3bf89a4a8efb6a4d4128d7d4cd
Your Name <youremail@example.com>
2025-03-09 22:43:15 +0000

    Refreshing new content

--- api/users/role.model.js
+++
@@ -1,8 +0,0 @@
-// ./api/users/role.model.js
-const ROLES = {
-  USER: "user",
-  ADMIN: "admin",
-  SUPER_ADMIN: "superadmin",
-};
-
-export default ROLES;
--- index.js
+++
@@ -1,9 +0,0 @@
-// index.js
-import "./common/env.js";
-import routes from "./routes.js";
-import ExpressServer from "./server.js";
-
-export default new ExpressServer()
-  .router(routes)
-  .listen(process.env.PORT)
-  .initCron();
